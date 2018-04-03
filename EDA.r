75180259a0f97a57b68718cf6ac4c8e968c1fe2f
bayoishola20 <bayoishola20@yahoo.com>
2018-04-03 04:25:11 +0100

    EDA

--- EDA.r
+++ EDA.r
@@ -8,4 +8,4 @@ library(RColorBrewer)
 data("midwest")
 
 # Scatter plot of total population versus known poverty population
-ggplot(midwest, aes(x = poptotal, y = poppovertyknown, color = state)) + geom_point() + scale_color_brewer(palette = 'Accent')
+ggplot(midwest, aes(x = poptotal, y = poppovertyknown, color = state)) + geom_point(size=4) + scale_color_brewer(palette = 'Accent')
