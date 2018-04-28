c1c6d4122700a24e5028fe9c813a396ae6894d45
bayoishola20 <bayoishola20@yahoo.com>
2018-04-28 21:22:10 +0100

    plot

--- EDA.r
+++ EDA.r
@@ -29,3 +29,13 @@ reddit <- read.csv('reddit.csv')
 table(reddit$employment.status)
 str(reddit)
 levels(reddit$age.range)
+
+
+qplot(data = reddit, x = age.range)
+qplot(data = reddit, x = income.range)
+
+# QUIZ
+
+levels(reddit$age.range)
+reddit$age.range <- ordered(reddit$age.range, levels = c("Under 18", "18-24", "35-44", "45-54", "55-64", "65 or Above"))
+qplot(data = reddit, x = age.range)
