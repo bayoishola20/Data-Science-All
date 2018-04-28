77e81ff1f76b9a83546d2cbdd6019a55a8b63383
bayoishola20 <bayoishola20@yahoo.com>
2018-04-28 18:54:03 +0100

    more

--- EDA.r
+++ EDA.r
@@ -8,4 +8,13 @@ library(RColorBrewer)
 data("midwest")
 
 # Scatter plot of total population versus known poverty population
-ggplot(midwest, aes(x = poptotal, y = poppovertyknown, color = state)) + geom_point(size=4) + scale_color_brewer(palette = 'Accent')
+ggplot(midwest, aes(x = poptotal, y = poppovertyknown, color = state)) + geom_point(size=5) + scale_color_brewer(palette = 'Accent')
+
+
+# ?mean - this opens help file
+# getwd()
+# View(diamonds)
+
+diamondsSubset <- diamonds[diamonds$color=="E", ]
+head(diamondsSubset, 2)
+dim(diamondsSubset)
