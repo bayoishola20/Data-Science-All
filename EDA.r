fb0ca9b6d8a394529f80e9c8a31835e709c0f16c
bayoishola20 <bayoishola20@yahoo.com>
2018-04-28 20:40:09 +0100

    plot

--- EDA.r
+++ EDA.r
@@ -18,3 +18,14 @@ ggplot(midwest, aes(x = poptotal, y = poppovertyknown, color = state)) + geom_po
 diamondsSubset <- diamonds[diamonds$color=="E", ]
 head(diamondsSubset, 2)
 dim(diamondsSubset)
+
+data(mtcars)
+str(mtcars)
+subset(mtcars, mpg >= 30 | hp < 60)
+mtcars[mtcars$mpg >= 30 | mtcars$hp < 60, ]
+
+setwd("/var/www/example.com/public_html/Data-Science-All")
+reddit <- read.csv('reddit.csv')
+table(reddit$employment.status)
+str(reddit)
+levels(reddit$age.range)
