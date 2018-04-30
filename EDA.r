997ea0a08fc114c41289925fcf40db9849c8c8d9
bayoishola20 <bayoishola20@yahoo.com>
2018-04-30 22:47:35 +0100

    Facebook pseudo-data

--- EDA.r
+++ EDA.r
@@ -39,3 +39,5 @@ qplot(data = reddit, x = income.range)
 levels(reddit$age.range)
 reddit$age.range <- ordered(reddit$age.range, levels = c("Under 18", "18-24", "35-44", "45-54", "55-64", "65 or Above"))
 qplot(data = reddit, x = age.range)
+
+read.delim('pseudo_facebook.tsv')
