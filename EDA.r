6571be20fc7423b8291679054561d9c95fb11821
bayoishola20 <bayoishola20@yahoo.com>
2018-05-02 21:13:39 +0100

    EDA

--- EDA.r
+++ EDA.r
@@ -54,4 +54,35 @@ library(ggthemes)
 
 ggplot(aes(x = dob_day), data = pf) +
   geom_histogram(binwidth = 1) +
-  scale_x_continuous(breaks = 1:31)
+  scale_x_continuous(breaks = 1:31) +
+  facet_wrap(~dob_month, ncol = 3)
+
+ggplot(aes(x = friend_count), data = subset(pf, !is.na(gender))) +
+  geom_histogram(binwidth = 25) +
+  scale_x_continuous(limits = c(0, 1000), breaks = seq(0,1000,50))+
+  facet_wrap(~gender)
+
+qplot(x = friend_count, data = pf, xlim = c(0, 1000))
+
+table(pf$gender)
+by(pf$friend_count, pf$gender, summary)
+
+qplot(x = tenure, data = pf, color = I('black'), fill = I('#FF9429'))
+
+qplot(x = tenure/365, data = pf, binwidth = .25, color = I('white'), fill = I('#FF9429')) +
+  scale_x_continuous(breaks = seq(1, 7, 1), limits = c(0, 7)) +
+  xlab('Number of years using Facebook') +
+  ylab('Number of users in sample')
+
+ggplot(aes(x = tenure), data = pf) +
+  geom_histogram(binwidth = 30, color = 'black', fill = 'skyblue')
+
+ggplot(aes(x = tenure/365), data = pf) +
+  geom_histogram(binwidth = .25, color = 'white', fill = 'lightblue')
+
+
+ggplot(aes(x = age), data = pf) +
+  geom_histogram(binwidth = 1, fill = '#7760AB') +
+  scale_x_continuous(breaks = seq(0, 113, 5)) +
+  xlab('Facebook users age') +
+  ylab('Number of users in sample')
